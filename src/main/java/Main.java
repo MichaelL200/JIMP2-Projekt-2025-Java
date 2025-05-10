2a2aee9f6f99350d1adebe95fdcdefd17232f6e0
Michał Ludwiczak <01197824@pw.edu.pl>
2025-05-10 14:29:12 +0200

    Add Start Screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import gui.StartScreen;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("GraphDivider");
+
+        // Start Screen
+        StartScreen start = new StartScreen();
+        start.getButton1().addActionListener(e -> { /* do A */ });
+        start.getButton2().addActionListener(e -> { /* do B */ });
+        start.setVisible(true);
     }
 }
